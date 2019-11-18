9503cd572a8dac168861d74c8048adb2aaaa87fd
Coursera Learner <nobody@nowhere.nul>
2019-11-18 09:05:17 +0000

    d

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,25 @@
 #include "cards.h"
-
+#include <stdio.h>
 int main(void) {
 
+  card_t c;
+  card_t c1;
+
+  c.value = 2;
+  c.suit = SPADES;
+  c1.value = 4;
+  c1.suit = DIAMONDS;
+
+  assert_card_valid(c);
+  assert_card_valid(c1);
+
+  char suit_c = suit_letter(c1);
+  printf("%s", &suit_c);
+
+  char value_c = value_letter(c1);
+  printf("%s", &value_c);
+
+  //print_card(c);
+  //print_card(c1);
+  return 0;
 }
